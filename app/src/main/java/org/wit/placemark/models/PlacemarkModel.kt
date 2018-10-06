560e213fc6a32355f79734ff95b4a92787ef3f55
Eamonn de Leastar <edeleastar@wit.ie>
2018-10-06 11:50:33 +0100

    support placemark description in activity + model

--- app/src/main/java/org/wit/placemark/models/PlacemarkModel.kt
+++ app/src/main/java/org/wit/placemark/models/PlacemarkModel.kt
@@ -1,5 +1,7 @@
 package org.wit.placemark.models
 
-data class PlacemarkModel(var title: String = "")
+data class PlacemarkModel(var title: String = "",
+                          var description: String = "")
+
 
 

32701ee809494d32527c7971b3c6af13a7c1af57
Eamonn de Leastar <edeleastar@wit.ie>
2018-10-06 11:15:12 +0100

    as generated by studio 3.2

---
+++ app/src/main/java/org/wit/placemark/PlacemarkActivity.kt
@@ -0,0 +1,12 @@
+package org.wit.placemark
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PlacemarkActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_placemark)
+  }
+}

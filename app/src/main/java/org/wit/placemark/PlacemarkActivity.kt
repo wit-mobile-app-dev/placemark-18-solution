517dd143c081a240bedd08a4e7218542ee3bbc3b
Eamonn de Leastar <edeleastar@wit.ie>
2018-10-06 11:36:54 +0100

    anko commons library+  log message at atartup

--- app/src/main/java/org/wit/placemark/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemark/PlacemarkActivity.kt
@@ -2,11 +2,14 @@ package org.wit.placemark
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 
-class PlacemarkActivity : AppCompatActivity() {
+class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_placemark)
+    info("Placemark Activity started..")
   }
 }

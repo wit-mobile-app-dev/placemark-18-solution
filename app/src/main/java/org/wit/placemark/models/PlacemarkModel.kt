9d5dc3442e46db4e15231694ab26d67e1a76b30d
Eamonn de Leastar <edeleastar@wit.ie>
2018-10-06 12:21:17 +0100

    make PlacemarkModel Parcelable

--- app/src/main/java/org/wit/placemark/models/PlacemarkModel.kt
+++ app/src/main/java/org/wit/placemark/models/PlacemarkModel.kt
@@ -1,7 +1,12 @@
 package org.wit.placemark.models
 
-data class PlacemarkModel(var title: String = "",
-                          var description: String = "")
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class PlacemarkModel(var id: Long = 0,
+                          var title: String = "",
+                          var description: String = "") : Parcelable
 
 
 

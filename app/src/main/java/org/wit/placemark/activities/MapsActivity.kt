c43f920a62e61e672817dd93767e4baba42df2c3
Eamonn de Leastar <edeleastar@wit.ie>
2018-10-24 10:08:58 +0200

    presenter + for MapsActivity

--- app/src/main/java/org/wit/placemark/activities/MapsActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/MapsActivity.kt
@@ -1,71 +1,44 @@
 package org.wit.placemark.activities
 
-import android.app.Activity
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
 import org.wit.placemark.R
-import org.wit.placemark.models.Location
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerDragListener, GoogleMap.OnMarkerClickListener {
+class MapsActivity : AppCompatActivity(), GoogleMap.OnMarkerDragListener, GoogleMap.OnMarkerClickListener {
 
-  private lateinit var map: GoogleMap
-  var location = Location()
+  lateinit var map: GoogleMap
+  lateinit var presenter: MapsPresenter
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_maps)
-    location = intent.extras.getParcelable<Location>("location")
-    val mapFragment = supportFragmentManager
-        .findFragmentById(R.id.map) as SupportMapFragment
-    mapFragment.getMapAsync(this)
+    val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+    presenter = MapsPresenter(this)
+    mapFragment.getMapAsync {
+      map = it
+      map.setOnMarkerDragListener(this)
+      map.setOnMarkerClickListener(this)
+      presenter.initMap(map)
+    }
   }
 
-  override fun onMapReady(googleMap: GoogleMap) {
-    map = googleMap
-    map.setOnMarkerDragListener(this)
-    map.setOnMarkerClickListener(this)
-    val loc = LatLng(location.lat, location.lng)
-    val options = MarkerOptions()
-        .title("Placemark")
-        .snippet("GPS : " + loc.toString())
-        .draggable(true)
-        .position(loc)
-    map.addMarker(options)
-    map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, location.zoom))
-  }
+  override fun onMarkerDragStart(marker: Marker) {}
 
-  override fun onMarkerDragStart(marker: Marker) {
-  }
-
-  override fun onMarkerDrag(marker: Marker) {
-  }
+  override fun onMarkerDrag(marker: Marker) {}
 
   override fun onMarkerDragEnd(marker: Marker) {
-    location.lat = marker.position.latitude
-    location.lng = marker.position.longitude
-    location.zoom = map.cameraPosition.zoom
+    presenter.doUpdateLocation(marker.position.latitude, marker.position.longitude, map.cameraPosition.zoom)
   }
 
   override fun onBackPressed() {
-    val resultIntent = Intent()
-    resultIntent.putExtra("location", location)
-    setResult(Activity.RESULT_OK, resultIntent)
-    finish()
-    super.onBackPressed()
+    presenter.doOnBackPressed()
   }
 
   override fun onMarkerClick(marker: Marker): Boolean {
-    val loc = LatLng(location.lat, location.lng)
-    marker.setSnippet("GPS : " + loc.toString())
+    presenter.doUpdateMarker(marker)
     return false
   }
 }

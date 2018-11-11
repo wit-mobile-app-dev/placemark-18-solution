601857a292eb4d9d29b907fd7db393123249d3b6
Eamonn de Leastar <edeleastar@wit.ie>
2018-11-11 15:46:13 +0000

    show email of logged in user in title bar

--- app/src/main/java/org/wit/placemark/views/BaseView.kt
+++ app/src/main/java/org/wit/placemark/views/BaseView.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Parcelable
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.google.firebase.auth.FirebaseAuth
 import org.jetbrains.anko.AnkoLogger
 
 import org.wit.placemark.models.PlacemarkModel
@@ -49,6 +50,10 @@ open abstract class BaseView() : AppCompatActivity(), AnkoLogger {
     toolbar.title = title
     setSupportActionBar(toolbar)
     supportActionBar?.setDisplayHomeAsUpEnabled(upEnabled)
+    val user = FirebaseAuth.getInstance().currentUser
+    if (user != null) {
+      toolbar.title = "${title}: ${user.email}"
+    }
   }
 
   override fun onDestroy() {

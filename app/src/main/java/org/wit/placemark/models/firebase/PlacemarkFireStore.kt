fce019b5b9d232774740aa7c1c2ebcdaa3377f7b
Eamonn de Leastar <edeleastar@wit.ie>
2018-11-18 12:29:46 +0000

    extend PlacemarkFireStore to upload image to stroage

--- app/src/main/java/org/wit/placemark/models/firebase/PlacemarkFireStore.kt
+++ app/src/main/java/org/wit/placemark/models/firebase/PlacemarkFireStore.kt
@@ -1,17 +1,24 @@
 package org.wit.placemark.models.firebase
 
 import android.content.Context
+import android.graphics.Bitmap
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import org.jetbrains.anko.AnkoLogger
+import org.wit.placemark.helpers.readImageFromPath
 import org.wit.placemark.models.PlacemarkModel
 import org.wit.placemark.models.PlacemarkStore
+import java.io.ByteArrayOutputStream
+import java.io.File
 
 class PlacemarkFireStore(val context: Context) : PlacemarkStore, AnkoLogger {
 
   val placemarks = ArrayList<PlacemarkModel>()
   lateinit var userId: String
   lateinit var db: DatabaseReference
+  lateinit var st: StorageReference
 
   suspend override fun findAll(): List<PlacemarkModel> {
     return placemarks
@@ -24,9 +31,12 @@ class PlacemarkFireStore(val context: Context) : PlacemarkStore, AnkoLogger {
 
   suspend override fun create(placemark: PlacemarkModel) {
     val key = db.child("users").child(userId).child("placemarks").push().key
-    placemark.fbId = key!!
-    placemarks.add(placemark)
-    db.child("users").child(userId).child("placemarks").child(key).setValue(placemark)
+    key?.let {
+      placemark.fbId = key
+      placemarks.add(placemark)
+      db.child("users").child(userId).child("placemarks").child(key).setValue(placemark)
+      updateImage(placemark)
+    }
   }
 
   suspend override fun update(placemark: PlacemarkModel) {
@@ -39,6 +49,9 @@ class PlacemarkFireStore(val context: Context) : PlacemarkStore, AnkoLogger {
     }
 
     db.child("users").child(userId).child("placemarks").child(placemark.fbId).setValue(placemark)
+    if ((placemark.image.length) > 0 && (placemark.image[0] != 'h')) {
+      updateImage(placemark)
+    }
   }
 
   suspend override fun delete(placemark: PlacemarkModel) {
@@ -50,17 +63,43 @@ class PlacemarkFireStore(val context: Context) : PlacemarkStore, AnkoLogger {
     placemarks.clear()
   }
 
+  fun updateImage(placemark: PlacemarkModel) {
+    if (placemark.image != "") {
+      val fileName = File(placemark.image)
+      val imageName = fileName.getName()
+
+      var imageRef = st.child(userId + '/' + imageName)
+      val baos = ByteArrayOutputStream()
+      val bitmap = readImageFromPath(context, placemark.image)
+
+      bitmap?.let {
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+        val uploadTask = imageRef.putBytes(data)
+        uploadTask.addOnFailureListener {
+          println(it.message)
+        }.addOnSuccessListener { taskSnapshot ->
+          taskSnapshot.metadata!!.reference!!.downloadUrl.addOnSuccessListener {
+            placemark.image = it.toString()
+            db.child("users").child(userId).child("placemarks").child(placemark.fbId).setValue(placemark)
+          }
+        }
+      }
+    }
+  }
+
   fun fetchPlacemarks(placemarksReady: () -> Unit) {
     val valueEventListener = object : ValueEventListener {
-      override fun onCancelled(error: DatabaseError) {
+      override fun onCancelled(dataSnapshot: DatabaseError) {
       }
       override fun onDataChange(dataSnapshot: DataSnapshot) {
-        dataSnapshot.children.mapNotNullTo(placemarks) { it.getValue<PlacemarkModel>(PlacemarkModel::class.java) }
+        dataSnapshot!!.children.mapNotNullTo(placemarks) { it.getValue<PlacemarkModel>(PlacemarkModel::class.java) }
         placemarksReady()
       }
     }
     userId = FirebaseAuth.getInstance().currentUser!!.uid
     db = FirebaseDatabase.getInstance().reference
+    st = FirebaseStorage.getInstance().reference
     placemarks.clear()
     db.child("users").child(userId).child("placemarks").addListenerForSingleValueEvent(valueEventListener)
   }
